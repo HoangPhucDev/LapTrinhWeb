e363397b5b163262d3eaca0040df549bdfc42aa8
HoangPhucDev <nhp1107@gmail.com>
2017-05-01 16:10:04 +0700

    Fix bug file index

--- default/index.php
+++ default/index.php
@@ -4,66 +4,76 @@ include_once '../class/Model.php';
     <div id="all">
         <div id="content">
             <?php include_once 'general/slider.php';?>               
-                    <div id="hot">
-               <?php                 
+               <?php     
+					
                     $data = new Model();
-                    $sql = $data->get_list("select * FROM `category`");
-              if(!empty($sql)){
-                   foreach ($sql as $key =>$value){
-                       echo ' <div class="box">
+                    $query = $data->get_list("select * FROM `category`");
+              
+   foreach ($query as $value){?>
+            <div id="hot">
+
+                <div class="box">
                     <div class="container">
                         <div class="col-md-12">
-                            <h2>'.$value['name'].'</h2>
+                            <h2><?php echo $value["name"]?></h2>
                         </div>
                     </div>
-                </div>';
-                   }
-              }
-              ?>
+                </div>
+
                 <div class="container">
                     <div class="product-slider">
-                             <!-- *** Load Sáº£n Pháº©m Trong Danh Má»¥c *** -->   
+                             <!-- *** Load Sản Phẩm Trong Danh Mục *** -->   
                     <?php
     
+
+    
+
                     $query2 = $data->get_list("SELECT * FROM `products` INNER JOIN `category_detail` ON products.category = category_detail.id  WHERE `category_id` =".$value["id"]." LIMIT 0,10");
-
-
-               foreach ($query2 as $value1){
-                 echo '<div class="item">
+					
+					
+               
+				foreach ($query2 as $value1){?>
+                    
+                        <div class="item">
                             <div class="product">
                                 <div class="flip-container">
                                     <div class="flipper">
                                         <div class="front">
-                                            <a href="detail.php?id='.$value1["id"].'">
-                                                <img src="img/'.$value1["image"].'" alt="" class="img-responsive">
+                                            <a href="detail.php?id=<?php echo $value["id"]?>">
+                                                <img src="img/<?php echo $value1["image"];?>" alt="" class="img-responsive">
                                             </a>
                                         </div>
                                         <div class="back">
-                                            <a href="detail.php?id='.$value1["id"].'">
-                                                <img src="img/'.$value1["image"].'" alt="" class="img-responsive">
+                                            <a href="detail.php?id=<?php echo $value1["id"]?>">
+                                                <img src="img/<?php echo $value1["image"];?>" alt="" class="img-responsive">
                                             </a>
                                         </div>
                                     </div>
                                 </div>
-                                <a href="detail.php?id='.$value1["id"].'" class="invisible">
-                                    <img src="img/'.$value1["image"].'" alt="" class="img-responsive">
+                                <a href="detail.php?id=<?php echo $value1["id"]?>" class="invisible">
+                                    <img src="img/<?php echo $value1["image"];?>" alt="" class="img-responsive">
                                 </a>
                                 <div class="text">
-                                    <h3><a href="detail.php?id='.$value1["id"].'">'.$value1["name"].'</a></h3>
-                                    <p class="price">'.number_format($value1['price']).' VNÄ�</p>
+                                    <h3><a href="detail.php?id=<?php echo $value1["id"]?>"><?php echo $value1["name"];?></a></h3>
+                                    <p class="price"><?php echo number_format($value1['price']) . " VNĐ";?></p>
                                 </div>
                                 <!-- /.text -->
                             </div>
                             <!-- /.product -->
-                        </div>' ;             
+                        </div>
 
-       }
+                         <?php } ?>
 
-?>
+                    </div>
+                    <!-- /.product-slider -->
+                </div>
+                <!-- /.container -->
 
-                       </div>
+            </div>
+            <!-- *** Kết Thúc Load 1 Danh Mục *** -->
 
-                     </div>
+                    <!-- /.product-slider -->
+             <?php } ?>        
                   </div>
                 </div>
             </div>
