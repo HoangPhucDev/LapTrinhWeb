e589714e94c6d87eb9d78e10f81f266cf82d64f2
HoangPhucDev <nhp1107@gmail.com>
2017-05-07 00:22:38 +0700

    làm xong kiểm tra đăng nhập trang admin, fix đăng ký

--- default/customer.php
+++ default/customer.php
@@ -5,13 +5,29 @@
    if (count($_POST) > 0){
        $fullname = $_POST['fullname'];
        $username = $_POST['username'];
-       $passwword= $_POST['password'];
-       $query = $data->insert(`customer`, array(`username` => ''.$username,`password` => ''.$passwword,`fullname` => ''.$fullname));
+       $passwword= md5($_POST['password']);
+       $phone = $_POST['phone'];
+       $email = $_POST['email'];
+       $address  = $_POST['address'];
+
+       $customer =  array('username' => ''.$username,
+                          'password' => ''.$passwword,
+                          'fullname' => ''.$fullname,
+                          'phone' => ''.$phone,
+                          'email' => ''.$email,
+                          'address' => ''.$address);
+       $query = $data->insert('customer',$customer);
+
        if($query){
            $id = 0;
            $row = $data->get_row("SELECT `id` FROM `customer` WHERE `username`='$username'");
            $id = $row['id'];
            $_SESSION['customer'] = $id;
+           header("Location: index.php");
+       }else
+       {
+        header("Location: register.php");
        }
-       header("Location: category.php");
+    
    }
+?>
